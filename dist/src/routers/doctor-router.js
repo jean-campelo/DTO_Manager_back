eeaa218a9fd17c06d02def7d083cb0243fd4e3be
Jean Campelo <jean93campelo@gmail.com>
2023-04-25 10:41:40 -0300

    remove dist and add to .env

--- dist/src/routers/doctor-router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import doctorController from "../controllers/doctor-controller.js";
-//const doctorRouter = Router();
-//doctorRouter.get("", doctorController.getDoctor);
-//export { doctorRouter };
-var router = Router();
-router.get("/doctos", doctorController.getDoctor);
-export default router;
